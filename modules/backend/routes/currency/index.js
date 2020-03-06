9b9c4cf0ccb275cea6362462bd0097ef25c4f8bc
Tsige wondwosen <wondwosen.shi@gmail.com>
2020-03-06 15:55:17 +0100

    Added Root end point.

--- modules/backend/routes/currency/index.js
+++ modules/backend/routes/currency/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./controller.currency');
+const totalcurr = require('./totalcurrency')
+
+router.get('/', totalcurr.totalcurrency)
 
 router.get('/convert', controller.convertCurrency);
 
 module.exports = router;
+
